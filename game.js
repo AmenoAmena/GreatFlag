7414257b59015fc6f70f59467eb25ddb377504fb
AmenoAmena <ardacavusoglu666@gmail.com>
2024-01-05 18:40:49 +0300

    using javascript

--- game.js
+++ game.js
@@ -1,10 +1,33 @@
-let countryName = document.getElementById("countryName").value;
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("form").addEventListener("submit", function (event) {
+        event.preventDefault();  
+        getFormValue();
+    });
+
+    function getFormValue() {
+        let countryName = document.getElementById("countryName").value;
+        let continent = document.getElementById("continent").value;
+        let gold = document.getElementById("gold").value;
+
+        console.log(countryName);
+        console.log(gold);
+        console.log(continent);
+
+        setLocalStorageCountryProperties(countryName);
+
+        updateParagraph(countryName);
+    }
+
+    function setLocalStorageCountryProperties(countryName) {
+        localStorage.setItem('countryName', countryName);
+    }
 
 
-function setLocalStorageCountryProperties() {
-    localStorage.setItem('countryName','countryName');
-}
+    getLocalStorageCountryProperties();
+});
+
 
 function getLocalStorageCountryProperties() {
-    localStorage.getItem("countryName");
-}                                                   
+    const storedCountryName = localStorage.getItem("countryName");
+    console.log(storedCountryName);
+}
