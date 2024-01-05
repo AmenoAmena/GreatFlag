9f961a69f882ffd6872b53b31279792ed93d4bc4
AmenoAmena <ardacavusoglu666@gmail.com>
2024-01-05 19:39:41 +0300

    Just 2 hour fun game now not cities skylnes clone

--- game.js
+++ game.js
@@ -1,1 +1,31 @@
-s
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        getFormValue();
+    });
+
+    function getFormValue() {
+        const countryName = document.getElementById("countryName").value;
+        const continent = document.getElementById("continent").value;
+        const gold = document.getElementById("gold").value;
+
+        // Validate input values
+        if (!countryName || !continent || !gold) {
+            alert("Please fill in all fields");
+            return;
+        }
+
+        document.getElementById("Form").onclick = function() {
+            window.location.href = "game.html";
+        };
+
+        console.log(countryName);
+        console.log(gold);
+        console.log(continent);
+
+        setLocalStorageCountryProperties(countryName);
+    }
+
+});
