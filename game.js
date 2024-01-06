c4edac9fca6c2d3f3d9622701db6ef20c6d5a07f
AmenoAmena <ardacavusoglu666@gmail.com>
2024-01-06 13:46:43 +0300

    counter system worked

--- game.js
+++ game.js
@@ -3,18 +3,41 @@ let factory = 0;
 let hospital = 0;
 let school = 0;
 let university = 0;
-let mediacalSchool = 0;
+let medicalSchool = 0;
 
-let houseChange = document.getElementById("house");
-let factoryChange = document.getElementById("factory");
-let hospitalChange = document.getElementById("hospital");
-let schoolChange = document.getElementById("school");
-let universityChange = document.getElementById("university");
-let mediacalSchoolChange = document.getElementById("medicalSchool");
+function setChange(event) {
 
-houseChange.innerHTML = "House: " + house;
-factoryChange.innerHTML = "Factory: " + factory;
-hospitalChange.innerHTML = "Hospital: " + hospital;
-schoolChange.innerHTML = "School: " + school;
-universityChange.innerHTML = "University: " + university;
-mediacalSchoolChange.innerHTML = "Medical School: " + mediacalSchool;
+    event.preventDefault();
+
+    
+
+    let houseChange = document.getElementById("house");
+    let factoryChange = document.getElementById("factory");
+    let hospitalChange = document.getElementById("hospital");
+    let schoolChange = document.getElementById("school");
+    let universityChange = document.getElementById("university");
+    let medicalSchoolChange = document.getElementById("medicalSchool");
+
+    let selectedProperty = document.getElementById("counterPlus").value;
+
+    if (selectedProperty === "house") {
+        house += 1;
+    } else if (selectedProperty === "factory") {
+        factory += 1;
+    } else if (selectedProperty === "hospital") {
+        hospital += 1;
+    } else if (selectedProperty === "school") {
+        school += 1;
+    } else if (selectedProperty === "university") {
+        university += 1;
+    } else if (selectedProperty === "medicalSchool") {
+        medicalSchool += 1;
+    }
+
+    houseChange.innerHTML = "House: " + house;
+    factoryChange.innerHTML = "Factory: " + factory;
+    hospitalChange.innerHTML = "Hospital: " + hospital;
+    schoolChange.innerHTML = "School: " + school;
+    universityChange.innerHTML = "University: " + university;
+    medicalSchoolChange.innerHTML = "Medical School: " + medicalSchool;
+}
