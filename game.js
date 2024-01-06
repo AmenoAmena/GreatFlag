dd1be82ec1dea516e42bdb35d303f977a109a291
AmenoAmena <ardacavusoglu666@gmail.com>
2024-01-06 12:40:22 +0300

    counter logic

---
+++ game.js
@@ -0,0 +1,20 @@
+let house = 0;
+let factory = 0;
+let hospital = 0;
+let school = 0;
+let university = 0;
+let mediacalSchool = 0;
+
+let houseChange = document.getElementById("house");
+let factoryChange = document.getElementById("factory");
+let hospitalChange = document.getElementById("hospital");
+let schoolChange = document.getElementById("school");
+let universityChange = document.getElementById("university");
+let mediacalSchoolChange = document.getElementById("medicalSchool");
+
+houseChange.innerHTML = "House: " + house;
+factoryChange.innerHTML = "Factory: " + factory;
+hospitalChange.innerHTML = "Hospital: " + hospital;
+schoolChange.innerHTML = "School: " + school;
+universityChange.innerHTML = "University: " + university;
+mediacalSchoolChange.innerHTML = "Medical School: " + mediacalSchool;
